80d663e4bb8a06e8bcc88007e2b62f92d86fa821
Vishal Thakur <55181061+astrovishalthakur@users.noreply.github.com>
2022-06-14 08:48:32 +0530

    first commit

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "DVC-CNN-TF-Pipeline-Demo"
+AUTHOR_USER_NAME = "astrovishalthakur"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -13,12 +13,12 @@ LIST_OF_REQUIREMENTS = []
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author=AUTHOR_USER_NAME,
+    author="astrovishalthakur",
     description="A small package for DVC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/{astrovishalthakur}/{DVC-CNN-TF-Pipeline-Demo}",
+    author_email="astrovishalthakur@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
