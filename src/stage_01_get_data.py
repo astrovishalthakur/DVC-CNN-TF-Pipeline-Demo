befb34ae10037d98e5012c10ea4da87385f6a915
astrovishalthakur <astrovishalthakur@gmail.com>
2022-06-16 08:18:08 +0530

    reformat stage01 files

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -41,13 +41,12 @@ def get_data(config_path):
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        get_data(config_path=parsed_args.config, params_path=parsed_args.params)
+        get_data(config_path=parsed_args.config)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
